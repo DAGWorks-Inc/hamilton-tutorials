9a8d5ea131fe9480e578ed84cd63977bc51da905
zilto <tjean@DESKTOP-V6JDCS2.localdomain>
2023-08-07 19:25:57 -0400

    added features computation

--- 2023-08-08/data_cleaning.py
+++ 2023-08-08/data_cleaning.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 
-from hamilton.function_modifiers import extract_columns, load_from, value, save_to, does, value
-
-
-def _sanitize_column_names(column_names: list[str]) -> list[str]:
-    return [col.lower() for col in column_names]
+from hamilton.function_modifiers import (
+    extract_columns,
+    load_from,
+    value,
+    save_to,
+)
 
 
 RAW_APPLICATION_RECORD_COLUMNS = [
@@ -18,38 +19,34 @@ RAW_APPLICATION_RECORD_COLUMNS = [
 
 @extract_columns(*RAW_APPLICATION_RECORD_COLUMNS)
 @load_from.csv(path=value("./data/application_record.csv"))
-def raw_application_record(data: pd.DataFrame) -> pd.DataFrame:
-    data = data.set_index("ID")
-    data.columns = _sanitize_column_names(data.columns)
-    return data
-
-
-RAW_CREDIT_RECORD_COLUMNS = ['months_balance', 'status']
-
-@extract_columns(*RAW_CREDIT_RECORD_COLUMNS)
-@load_from.csv(path=value("./data/credit_record.csv"))
-def raw_credit_record(data: pd.DataFrame) -> pd.DataFrame:
-    data = data.set_index("ID")
-    data.columns = _sanitize_column_names(data.columns)
-    return data
+def raw_application_record(raw_application_csv: pd.DataFrame) -> pd.DataFrame:
+    """Load the raw application data from CSV; rename columns to lowercase;  set ID as index"""
+    raw_application_csv.columns = [col.lower() for col in raw_application_csv.columns]
+    raw_application_csv = raw_application_csv.set_index("id")
+    raw_application_csv = raw_application_csv.groupby("id").first()
+    return raw_application_csv
 
 
 def gender_encoded(code_gender: pd.Series) -> pd.Series:
+    """Encode code_gender as int"""
     encoding = {"M": 0, "F": 1}
     return code_gender.replace(encoding)
 
 
 def own_car_encoded(flag_own_car: pd.Series) -> pd.Series:
+    """encode flag_own_car as int"""
     encoding = {"N": 0, "Y": 1}
     return flag_own_car.replace(encoding)
 
 
 def own_realty_encoded(flag_own_realty: pd.Series) -> pd.Series:
+    """encode flag_own_realty as int"""
     encoding = {"N": 0, "Y": 1}
     return flag_own_realty.replace(encoding)
 
 
 def eduction_type_encoded(name_education_type: pd.Series) -> pd.Series:
+    """encode name_education_type as int"""
     encoding = {
         'Lower secondary': 0,
         'Secondary / secondary special': 1,
@@ -61,6 +58,7 @@ def eduction_type_encoded(name_education_type: pd.Series) -> pd.Series:
 
 
 def income_type_encoded(name_income_type: pd.Series) -> pd.Series:
+    """encode name_income_type as int"""
     encoding = {
         'Student': 0,
         'Working': 1,
@@ -72,6 +70,7 @@ def income_type_encoded(name_income_type: pd.Series) -> pd.Series:
 
 
 def family_status_encoded(name_family_status: pd.Series) -> pd.Series:
+    """encode name_family_status as int"""
     encoding = {
         'Single / not married': 0,
         'Married': 1,
@@ -83,6 +82,7 @@ def family_status_encoded(name_family_status: pd.Series) -> pd.Series:
 
 
 def housing_type_encoded(name_housing_type: pd.Series) -> pd.Series:
+    """encode name_housing_type as int"""
     encoding = {
         'With parents': 0,
         'Rented apartment': 1,
@@ -95,6 +95,7 @@ def housing_type_encoded(name_housing_type: pd.Series) -> pd.Series:
 
 
 def occupation_type_encoded(occupation_type: pd.Series) -> pd.Series:
+    """encode occupation_type as int"""
     encoding = {
         np.nan: 0,  # np.nan indicates unemployed
         'Laborers': 1,
@@ -119,12 +120,7 @@ def occupation_type_encoded(occupation_type: pd.Series) -> pd.Series:
     return occupation_type.replace(encoding)
 
 
-def _join_series(**series) -> pd.DataFrame:
-    return pd.DataFrame(dict(**series))
-
-
 @save_to.parquet(path=value("./data/application_clean.parquet"), output_name_="save_application_clean")
-@does(_join_series)
 def application_clean(
     cnt_children: pd.Series,
     amt_income_total: pd.Series,
@@ -144,6 +140,37 @@ def application_clean(
     own_car_encoded: pd.Series,
     own_realty_encoded: pd.Series,
 ) -> pd.DataFrame:
-    pass
+    """Join cleaned series into dataframe and save to parquet"""
+    _df = pd.DataFrame.from_dict(locals())
+    return _df
 
 
+RAW_CREDIT_RECORD_COLUMNS = ['months_balance', 'status']
+
+@extract_columns(*RAW_CREDIT_RECORD_COLUMNS)
+@load_from.csv(path=value("./data/credit_record.csv"))
+def raw_credit_record(raw_credit_csv: pd.DataFrame) -> pd.DataFrame:
+    """Load the raw credit data from CSV; rename columns to lowercase;  set ID as index"""
+    raw_credit_csv.columns = [col.lower() for col in raw_credit_csv.columns]
+    raw_credit_csv = raw_credit_csv.set_index("id")
+    return raw_credit_csv
+
+
+def month_index(raw_credit_record: pd.DataFrame) -> pd.Series:
+    """Convert `months_balance` from -3, -2, 1, 0 to 0, 1, 2, 3"""
+    return (
+        raw_credit_record["months_balance"]
+        .groupby("id")
+        .apply(lambda x: x - x.min())
+        .reset_index(level=0, drop=True)
+    )
+
+
+@save_to.parquet(path=value("./data/credit_clean.parquet"), output_name_="save_credit_clean")
+def credit_clean(
+    month_index: pd.Series,
+    status: pd.Series,
+) -> pd.DataFrame:
+    """Join cleaned series into dataframe and save to parquet"""
+    _df = pd.DataFrame.from_dict(locals())
+    return _df
--- 2023-08-08/features.py
+++ 2023-08-08/features.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 
-from hamilton.function_modifiers import extract_columns, load_from, value
+from hamilton.function_modifiers import extract_columns, extract_fields, load_from, save_to, value
 
 
 CLEAN_APPLICATION_RECORD_COLUMNS = [
@@ -13,14 +14,182 @@ CLEAN_APPLICATION_RECORD_COLUMNS = [
 
 @extract_columns(*CLEAN_APPLICATION_RECORD_COLUMNS)
 @load_from.parquet(path=value("./data/application_clean.parquet"))
-def clean_application_record(data: pd.DataFrame) -> pd.DataFrame:
-    return data
+def clean_application_record(clean_application_parquet: pd.DataFrame) -> pd.DataFrame:
+    """Load the clean application records; results from data_cleaning.application_clean"""
+    return clean_application_parquet
 
 
-def has_children(cnt_children: pd.Series) -> pd.Series:
-    return pd.Series(np.where(cnt_children>0, 1, 0), index=cnt_children.index)
+CLEAN_CREDIT_RECORD_COLUMNS = ['month_index', 'status']
+
+@extract_columns(*CLEAN_CREDIT_RECORD_COLUMNS)
+@load_from.parquet(path=value("./data/credit_clean.parquet"))
+def clean_credit_record(clean_credit_parquet: pd.DataFrame) -> pd.DataFrame:
+    """Load the clean credit records; results from data_cleaning.credit_clean"""
+    return clean_credit_parquet
+
+
+SALARIES_COLUMNS = ["all_salaries"]
+
+@extract_columns(*SALARIES_COLUMNS)
+@load_from.parquet(path=value("./data/salaries.parquet"))
+def salaries_record(salaries_parquet: pd.DataFrame) -> pd.DataFrame:
+    """Load salary data from external sources, with more users"""
+    salaries_parquet = salaries_parquet.rename(columns={"salary": "all_salaries"})
+    return salaries_parquet
 
 
 def age_in_years(days_birth: pd.Series) -> pd.Series:
+    """Convert days since birth into number of years"""
     return days_birth.abs().div(365).astype(int)
 
+
+def age_binned(age_in_years: pd.Series) -> pd.Series:
+    """Bin ages by decade; users must be at least 18"""
+    bins = [18, 30, 40, 50, 60, 70, 80]
+    binned_values = pd.cut(age_in_years, bins, right=False)
+    return binned_values.cat.codes.astype(int)
+
+
+@extract_fields(dict(
+    income_bins=np.ndarray,
+    income_percentile=pd.Series,
+))
+def income_percentile_(amt_income_total: pd.Series) -> dict:
+    """Percentile of total income computed on the main dataset"""
+    percentile, bins = pd.qcut(amt_income_total, 100, duplicates="drop", retbins=True)
+    return dict(
+        income_bins=bins,
+        income_percentile=percentile.cat.codes.astype(int),
+    )
+
+
+@extract_fields(dict(
+    salary_bins=np.ndarray,
+    salary_percentile=pd.Series,
+))
+def salary_percentile_(clean_application_record: pd.DataFrame, all_salaries: pd.Series) -> dict:
+    """Percentile of the salary computed on the external dataset"""
+    percentile, bins = pd.qcut(all_salaries, 100, duplicates="drop", retbins=True)
+    percentile_df = pd.Series(percentile.cat.codes, name="salary_percentile").to_frame()
+
+    merged_df = pd.merge(
+        clean_application_record, percentile_df,
+        how="left",
+        left_index=True, right_index=True,
+    )
+
+    merged_df.salary_percentile = merged_df.salary_percentile.fillna(0)
+    
+    return dict(
+        salary_bins=bins,
+        salary_percentile=pd.Series(merged_df.salary_percentile, dtype=int, index=clean_application_record.index),
+    )
+
+
+def years_employed(days_employed: pd.Series) -> pd.Series:
+    """Convert days employed into days employed"""
+    return days_employed.abs().div(365).astype(int)
+
+
+def has_children(cnt_children: pd.Series) -> pd.Series:
+    """Convert the number of children into a binary `has_children` yes/no"""
+    return pd.Series(np.where(cnt_children>0, 1, 0), index=cnt_children.index)
+
+
+def has_at_least_one_loan(
+    clean_application_record: pd.DataFrame,
+    status: pd.Series
+) -> pd.Series:
+    """Get the ID of users with at least one loan, i.e., not only X status
+    NOTE be careful to not introduce data leakage in predictive task
+    """
+    user_ids_with_loan = status[status!="X"].index.unique()
+    
+    loan_mask = clean_application_record.index.isin(user_ids_with_loan)
+
+    clean_application_record["has_at_least_one_loan"] = 0
+    clean_application_record.loc[loan_mask, "has_at_least_one_loan"] = 1
+    return clean_application_record.has_at_least_one_loan
+
+
+def number_of_credit_months(clean_application_record: pd.DataFrame, month_index: pd.Series) -> pd.Series:
+    """Total number of months of credit history per user"""
+    user_max = month_index.groupby("id").max()
+    merged_df = pd.merge(
+        clean_application_record, user_max.to_frame(),
+        how="left",
+        left_index=True, right_index=True,
+    )
+    merged_df.month_index = merged_df.month_index.fillna(0)
+
+    return pd.Series(merged_df.month_index, dtype=int, index=clean_application_record.index)
+
+
+@save_to.parquet(path=value("./data/features.parquet"), output_name_="save_features")
+def feature_set(
+    eduction_type_encoded: pd.Series,
+    family_status_encoded: pd.Series,
+    housing_type_encoded: pd.Series,
+    income_type_encoded: pd.Series,
+    occupation_type_encoded: pd.Series,
+    own_car_encoded: pd.Series,
+    own_realty_encoded: pd.Series,
+    number_of_credit_months: pd.Series,
+    age_binned: pd.Series,
+    income_percentile: pd.Series,
+    has_children: pd.Series,
+    salary_percentile: pd.Series,
+    years_employed: pd.Series,
+    cnt_fam_members: pd.Series,
+) -> pd.DataFrame:
+    """Join features into dataframe and save to parquet"""
+    _df = pd.DataFrame.from_dict(locals())
+    return _df
+
+
+def days_due(status: pd.Series) -> pd.Series:
+    """Encode status label into number of days as a regression target. 
+    Original values indicate:
+        X: No loan for the month
+        C: paid off that month
+        0: 1-29 days past due
+        1: 30-59 days past due
+        2: 60-89 days overdue
+        3: 90-119 days overdue
+        4: 120-149 days overdue
+        5: Overdue or bad debts, write-offs for more than 150 days
+    """
+    encoding = {
+        "X": -10,
+        "C": 0,
+        "0": 15,
+        "1": 45,
+        "2": 75,
+        "3": 105,
+        "4": 135,
+        "5": 165,
+    }
+    return status.replace(encoding).astype(int)
+
+
+@extract_fields(dict(
+    days_due_mean=pd.Series,
+    days_due_std=pd.Series,
+))
+def days_due_agg(clean_application_record: pd.DataFrame, days_due: pd.Series) -> dict:
+    """Compute average and standard deviation of `days_due` per user"""
+    aggregates = days_due.groupby("id").agg(["mean", "skew"])
+    return dict(
+        days_due_mean=aggregates[("days_due", "mean")],
+        std=aggregates[("days_due", "std")],
+    )
+
+
+@save_to.parquet(path=value("./data/targets.parquet"), output_name_="save_targets")
+def targets_set(
+    days_due_mean: pd.Series,
+    days_due_std: pd.Series,
+) -> pd.DataFrame:
+    """Join features into dataframe and save to parquet"""
+    _df = pd.DataFrame.from_dict(locals())
+    return _df
